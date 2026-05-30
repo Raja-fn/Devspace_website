6ce60a5a47d31459e0ad216ac30b68597eff7435
Raja-fn <businessrexxon@gmail.com>
2026-05-30 23:03:40 +0530

    fixed some login error

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import type { NextConfig } from "next";
 
-module.exports = nextConfig;
+const nextConfig: NextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "hybvsgxqstnxamdkijsk.supabase.co",
+            },
+        ],
+    },
+};
+
+export default nextConfig;
